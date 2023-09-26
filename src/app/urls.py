8f3d51d5c26377e1233808c1294f4b06dad673c5
Mateus Ferreira <mateusasferreira28@gmail.com>
2023-09-26 09:44:42 -0300

    feat: user registration

--- src/app/urls.py
+++ src/app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("registration/", views.registration, name="registration"),
+    path("signup/", views.create_user, name="signup"),
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -1,6 +1,23 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
+
+from app.models import User
 
 # Create your views here.
 def index(request):
     return HttpResponse('Hello World')
+
+def registration(request):
+    return render(request, 'registration/signup.html')
+
+def create_user(request: HttpRequest):
+    if len(request.POST.keys()) == 0:
+        return redirect("/registration")
+
+    password =  request.POST["password"]
+    email = request.POST["email"]
+    username = request.POST["username"]
+
+    User.objects.create_user(password=password, username=username, email=email)
+
+    return redirect("/")
